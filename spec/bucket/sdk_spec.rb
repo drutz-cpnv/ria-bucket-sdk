9e7e136130168b552d56af319824113c83b356b7
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2025-03-22 10:14:22 +0100

    Initial commit

---
+++ spec/bucket/sdk_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Bucket::Sdk do
+  it "has a version number" do
+    expect(Bucket::Sdk::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
